bc7a2caed0d0fe4bc1fd0ab742437f6b7080f32d
PyEikenWhize <mkimura.eiken@gmail.com>
2023-09-01 14:30:00 +0000

    Updated code

--- app.py
+++ app.py
@@ -1,32 +1,42 @@
 import streamlit as st
+import requests
+import pandas as pd
 
-def get_img(numbers):
-  for i in range(numbers):
-    # Step 1: Import necessary libraries
-    import requests  # Library for making HTTP requests
-    #from IPython.display import Image, display  # Library for displaying images
+def get_weather(city):
+    # Step 1: Define the API URL for OpenMeteo
+    api_url = f"https://api.open-meteo.com/v1/forecast?city={city}&daily=temperature_2m_min,daily_temperature_2m_max,precipitation_sum"
+    api_url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m"
 
-    # Step 2: Define the API URL for a random cat image
-    api_url = "https://api.thecatapi.com/v1/images/search"
-
-    # Step 3: Send a GET request to the API and get the response
+    # Step 2: Send a GET request to the OpenMeteo API
     response = requests.get(api_url)
 
-    # Step 4: Check if the request was successful (status code 200)
+    # Step 3: Check if the request was successful (status code 200)
     if response.status_code == 200:
-        # Step 5: Parse the JSON response to get the image URL
-        cat_data = response.json()
-        cat_image_url = cat_data[0]["url"]
+        # Step 4: Parse the JSON response to get weather data
+        weather_data = response.json()
+        print(weather_data.keys())
+        print(weather_data['hourly'])
 
-        # Step 6: Display the cat image inline in Colab
-        st.image(cat_image_url)
-    #    display(Image(url=cat_image_url))
+        # Step 5: Display the weather information
+        st.write(f"Weather information for {city}:")
+        st.write(f"Time: {weather_data['hourly']['time']}")
+        st.write(f"Temperature: {weather_data['hourly']['temperature_2m']}°C")
+        df = pd.DataFrame(weather_data['hourly']['temperature_2m'],weather_data['hourly']['time'])
+        st.title("Weather Data")
+        st.write("Sample weather data in a Pandas DataFrame:")
+        #print(df)
+        st.line_chart(df)
     else:
-        print("Failed to fetch cat data from the API.")
+        st.write(f"Failed to fetch weather data for {city}. Please check the city name.")
 
-st.write("Cat Image!")
-numbers = st.number_input('Insert a number', value = 3, step=1)
+#get_weather(city)
 
-if st.button("Load"):
-  st.write('The number is ', numbers)
-  get_img(numbers)
+# Streamlit UI elements
+st.title("OpenMeteo Weather App")
+st.write("Enter the city for weather information:")
+
+city = st.text_input("City Name", "Berlin")  # Default city is Berlin
+
+if st.button("Get Weather"):
+    st.write(f"Fetching weather information for {city}...")
+    get_weather(city)
