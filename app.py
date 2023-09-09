008c626b00d216385f8834f9850a86d1580308c1
USERNAME <MAIL>
2023-09-09 07:53:31 +0000

    Updated code

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import requests
 import pandas as pd
+import numpy as np
 
 def get_weather(latitude, longitude):
     # Step 1: Define the API URL for OpenMeteo
-    print(latitude, longitude)
     api_url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m"
 
     # Step 2: Send a GET request to the OpenMeteo API
@@ -14,29 +14,30 @@ def get_weather(latitude, longitude):
     if response.status_code == 200:
         # Step 4: Parse the JSON response to get weather data
         weather_data = response.json()
-        print(weather_data.keys())
-        print(weather_data['hourly'])
 
         df = pd.DataFrame(weather_data['hourly']['temperature_2m'],weather_data['hourly']['time'])
         st.title("Weather Data")
         st.write("Sample weather data in a Pandas DataFrame:")
         st.line_chart(df)
 
-        # Step 5: Display the weather information
-        st.write(f"Weather information for Latidude:{latitude}, Longitude:{longitude}:")
-        st.write(f"Time: {weather_data['hourly']['time']}")
-        st.write(f"Temperature: {weather_data['hourly']['temperature_2m']}°C")
+        # Step 5: Display the map
+        st.write(f"Weather information for Latidude: {latitude}, Longitude: {longitude}:")
+        df = pd.DataFrame(
+          np.random.randn(1000, 2) / [50, 50] + [latitude, longitude],
+          columns=['lat', 'lon'])
+        st.map(df)
+
     else:
         st.write(f"Failed to fetch weather data for Latidude:{latitude}, Longitude:{longitude}. Please check the city name.")
 
-#get_weather(38 ,134)
+#get_weather(35.0 ,135.7)
 
 # Streamlit UI elements
 st.title("OpenMeteo Weather App")
 st.write("Enter the city for weather information:")
 
-latitude = st.number_input("Latitude", value=36)
-longitude = st.number_input("Longitude", value=135)
+latitude = st.number_input("Latitude", value=35.0, step=0.1)
+longitude = st.number_input("Longitude", value=135.7, step=0.1)
 
 if st.button("Get Weather"):
     st.write(f"Fetching weather information for Latidude:{latitude}, Longitude:{longitude}...")
